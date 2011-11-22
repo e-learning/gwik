56b849ff41489d89718fb5c282257b8c2ee43000
Shulepov <mike1190@mail.ru>
2011-11-22 22:40:50 +0400

    Add image from wikipage

---
+++ gwik-main/src/java/gwik/osm/ImageInfoParser.java
@@ -0,0 +1,71 @@
+package gwik.osm;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: Shulepov
+ * Date: 09.11.11
+ */
+public class ImageInfoParser {
+    public void parse(String urlStr, ImagedWikiObject result){
+                HttpURLConnection urlc = null;
+                try {
+                    URL url = new URL(urlStr);
+                    urlc = (HttpURLConnection) url.openConnection();
+                    parse(urlc.getInputStream(), result);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    if (urlc != null) {
+                        urlc.disconnect();
+                    }
+                }
+            }
+
+            public void parse(InputStream is, ImagedWikiObject result) throws IOException {
+                try {
+                    Document doc = parserXML(is);
+
+                    extractImageInfo(doc, result);
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    is.close();
+                }
+            }
+
+            private void extractImageInfo(Document doc, ImagedWikiObject result){
+                Node imUrl = doc.getElementsByTagName("ii").item(0);
+
+                Node attrTitle = null;
+                try{
+                    NamedNodeMap attrs = imUrl.getAttributes();
+                    attrTitle = attrs.getNamedItem("url");
+
+                }catch(Exception e){
+                    throw new RuntimeException(e);
+                }
+
+                //если изображение найдено, установить ссылку на него
+                if ( attrTitle != null ){
+                    result.setImageUrl( attrTitle.getNodeValue() );
+                }
+
+            }
+
+            private Document parserXML(InputStream is) throws SAXException, IOException, ParserConfigurationException {
+                return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(is);
+            }
+
+}
---
+++ gwik-main/src/java/gwik/osm/ImagesXmlParser.java
@@ -0,0 +1,74 @@
+package gwik.osm;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : Shulepov
+ * Date: 09.11.11
+ */
+public class ImagesXmlParser {
+        public String parse(String urlStr){
+            HttpURLConnection urlc = null;
+            try {
+                URL url = new URL(urlStr);
+                urlc = (HttpURLConnection) url.openConnection();
+                return parse(urlc.getInputStream());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (urlc != null) {
+                    urlc.disconnect();
+                }
+            }
+        }
+
+        public String parse(InputStream is) throws IOException {
+            String result = new String();
+            try {
+                Document doc = parserXML(is);
+
+                result = extractImages(doc);
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            } finally {
+                is.close();
+            }
+            return result;
+        }
+
+        private String extractImages(Document doc){
+            NodeList images = doc.getElementsByTagName("im");
+            Node imNode = images.item(0);
+
+            Node attrTitle = null;
+            try{
+               NamedNodeMap attrs = imNode.getAttributes();
+               attrTitle = attrs.getNamedItem("title");
+            }catch(Exception e){
+                throw new RuntimeException(e);
+            }
+
+            if ( attrTitle != null) {
+                return  ( attrTitle.getNodeValue() );
+            }
+            else {
+                return "error";
+            }
+        }
+
+        private Document parserXML(InputStream is) throws SAXException, IOException, ParserConfigurationException {
+            return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(is);
+        }
+
+}
